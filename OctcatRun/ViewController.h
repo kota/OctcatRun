4aed70d293ac74dc4f638a52541a7ccbc096de67
Kota Fujiwara <fujiwara.kota@moneyforward.co.jp>
2015-01-16 19:16:38 +0900

    Initial Commit

---
+++ OctcatRun/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OctcatRun
+//
+//  Created by fujiwara.kota on 2015/01/16.
+//  Copyright (c) 2015年 Moneyforward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OctcatRun/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OctcatRun
+//
+//  Created by fujiwara.kota on 2015/01/16.
+//  Copyright (c) 2015年 Moneyforward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
